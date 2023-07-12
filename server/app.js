81c8f4d81eeadb000ea61e671ad5938935d003d6
paulornr89 <paulornr89@gmail.com>
2023-07-12 14:00:49 -0300

    implementado middleware para tratar erros

--- server/app.js
+++ server/app.js
@@ -6,6 +6,28 @@ app.get('/', (req, res) => {
     res.send('Serviço ativo');
 })
 
+// app.use((req, res, next) => {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// })
+app.use((req, res, next) => {
+    if(req.url === '/favicon.ico'){
+        res.writeHead(200, {'Content-Type': 'image/x-icon'})
+        res.end('');
+    }else{
+        next()
+    }
+})
+
+app.use((err, req, res, next) => {
+    if(err.status !== 404){
+        console.log(err.stack);
+        res.status(err.status || 500).json({err:err.message});
+    }    
+})
+
 /**PORTA */
 app.listen('3000');
+
 
